192a54ff8ba82e7554b9f43cb2b8a57d5f92bba4
Daniel Krikun <krikun.daniel@gmail.com>
2016-06-27 21:13:02 +0300

    Initial report: sketch main

---
+++ src/skv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Very simple key-value configuration service
+"""
+
+__version__ = '0.1.0'
+__license__ = 'MIT'
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main())
